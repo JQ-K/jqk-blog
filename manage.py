4bcf20631a8a9d8d9eb1cedea83d0dd62d4d08a7
JQK┏ (゜ω゜)=☞ <kangqingjie01@gmail.com>
2017-03-05 14:48:56 +0800

    运行程序

---
+++ manage.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from flask_script import Manager
+from flask_migrate import Migrate, init, upgrade, migrate
+
+from app import create_app, db
+
+app = create_app('development')
+manager = Manager(app)
+x_migrate = Migrate(app, db)
+
+
+@manager.command
+def create_db():
+    init()
+    upgrade_db()
+
+
+@manager.command
+def upgrade_db():
+    migrate()
+    upgrade()
+
+if __name__ == '__main__':
+    manager.run()
